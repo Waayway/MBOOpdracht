c66731ec1eb91eebe878b9095714d1b7315fc65d
Waayway <thijs@vanwaaij.com>
2022-04-13 15:23:30 +0200

    fixed phone mode

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
 
 <main class="bg-primary-darker h-screen w-screen flex flex-col justify-center items-center text-text">
   <h1 class="text-xl">Calculator</h1>
-  <div class="w-full h-3/4 pt-2 px-28">
+  <div class="w-full h-3/4 pt-2 md:px-28 px-2">
     <div class="w-full h-full bg-primary-medium rounded-3xl">
       <Calculator />
     </div>
